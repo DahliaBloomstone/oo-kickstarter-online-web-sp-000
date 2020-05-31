8476886deb39bccb8482348a8affc511df8bc8a9
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 01:03:00 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,11 +5,12 @@ attr_accessor :name, :backed_projects
 def initialize(name) #takes a name on initialization, accessible through an attribute reader, initializes with a @backed_projects attribute, an empty array
   @name = name
   @backed_projects = []
+end 
 
 def back_project(project)
   @backed_projects << project
   project.backers << self
-  
+
 
 
 end
