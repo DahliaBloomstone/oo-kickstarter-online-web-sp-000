ad7c690571061dba515ac07ded21aac993bdaa41
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 00:59:12 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,7 @@
 class Project
 
 attr_reader :backers, :title
+
 #takes a title on initialization, accessible through an attribute reader, initializes with a @backers attribute, an empty array
 def initialize(title)
   @title = title
